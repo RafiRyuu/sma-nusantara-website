12090c9b0482392f8e996aa56304695db0dd4659
RafiRyuu <ahmadrafihpp28@gmail.com>
2026-03-04 22:55:27 +0700

    Configure GitHub Pages deployment via GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/sma-nusantara-website/',
     root: './',
     publicDir: 'public',
     build: {
